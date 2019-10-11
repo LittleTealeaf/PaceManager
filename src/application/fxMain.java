e231a4cdef1c66a3f9a5ee8447440648f963a5ca
Tealeaf <ShintaoMonk@gmail.com>
2019-10-11 14:09:26 -0400

    Created Time.java

---
+++ src/application/fxMain.java
@@ -0,0 +1,16 @@
+package application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class fxMain extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void open(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/application/paceManager.java
@@ -0,0 +1,13 @@
+package application;
+
+import classes.*;
+
+public class paceManager {
+
+	
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
