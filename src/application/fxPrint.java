03dcf015d77c29465ab9f091164be60ef0c8c416
Tealeaf <35083315+LittleTealeaf@users.noreply.github.com>
2019-10-23 12:19:46 -0400

    Added notes to fxPrint.java

--- src/application/fxPrint.java
+++ src/application/fxPrint.java
@@ -1,10 +1,21 @@
 package application;
 
 import classes.*;
+import javafx.stage.Stage;
 
 public class fxPrint {
+	
+	/*
+	 * Documentation
+	 * http://www.java2s.com/Code/Java/2D-Graphics-GUI/PrintinJavapageformatanddocument.htm
+	 * https://docs.oracle.com/javase/tutorial/uiswing/misc/printtable.html
+	 */
+	
+	public static Stage sPrint;
 
 	public static void open() {
+		if(sPrint != null) sPrint.close();
+		sPrint = new Stage();
 		
 	}
 }
