0faacf8551d35b0d1a3ed3883c60021192a730d4
Tealeaf <35083315+LittleTealeaf@users.noreply.github.com>
2019-10-24 13:19:49 -0400

    Swapped to use the javafx print option
    Test with the printer at home, don't wanna print it at school

--- src/application/fxPrint.java
+++ src/application/fxPrint.java
@@ -1,28 +1,30 @@
 package application;
 
-import java.awt.BasicStroke;
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.FontMetrics;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.geom.Rectangle2D;
-import java.awt.print.Book;
-import java.awt.print.PageFormat;
-import java.awt.print.Printable;
-import java.awt.print.PrinterJob;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import classes.*;
+import javafx.collections.ObservableSet;
+import javafx.print.Printer;
+import javafx.print.PrinterJob;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.stage.Stage;
 
 public class fxPrint {
+	
+	private final static int POINTS_PER_INCH = 72;
 	
 	/*
 	 * Documentation
 	 * http://www.java2s.com/Code/Java/2D-Graphics-GUI/PrintinJavapageformatanddocument.htm
+	 * 
+	 * printing tables
 	 * https://docs.oracle.com/javase/tutorial/uiswing/misc/printtable.html
 	 */
 
@@ -33,26 +35,47 @@ public class fxPrint {
 		aPrint.setHeaderText("Select the preset method, or customize");
 		
 		//Add Buttons
+		ButtonType bTeams = new ButtonType("All Teams");
 		ButtonType bScoreBoard = new ButtonType("Scoreboard");
 		ButtonType bPrizes = new ButtonType("Prizes");
 		ButtonType bCustom = new ButtonType("Custom");
 		ButtonType bCancel = new ButtonType("Cancel");
 		
-		aPrint.getButtonTypes().addAll(bScoreBoard,bPrizes,bCustom,bCancel);
+		aPrint.getButtonTypes().addAll(bTeams,bScoreBoard,bPrizes,bCustom,bCancel);
 		
+		//Gets the result
 		Optional<ButtonType> res = aPrint.showAndWait();
-		if(res.get() == bScoreBoard) {
+		
+		//ObservableSet<Printer> printers = Printer.getAllPrinters();
+		
+		PrinterJob job = PrinterJob.createPrinterJob();
+		if(job == null) return;
+		
+		//Depending on the print option
+		//uses String[] args
+		if(res.get() == bTeams) {
+			
+		} else if(res.get() == bScoreBoard) {
 			
 		} else if(res.get() == bPrizes) {
 			
 		} else if(res.get() == bCustom) {
 			
 		} else {
-			
+			return; //Cancels Print
+		}
+	}
+	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public static TableView getTeamTable(String[] columns, List<Team> teams) {
+		TableView<Team> tview = new TableView<Team>();
+		
+		for(String s : columns) {
+			TableColumn col = new TableColumn(s);
+			col.setCellValueFactory(new PropertyValueFactory<Team,String>(s));
+			tview.getColumns().add(col);
 		}
 		
-		PrinterJob printJob = PrinterJob.getPrinterJob();
-		
-		
+		return tview;
 	}
 }
