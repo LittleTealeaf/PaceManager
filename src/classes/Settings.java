722afae84da6d48b9930265e4b933ea873064231
Tealeaf <35083315+LittleTealeaf@users.noreply.github.com>
2019-11-07 10:07:05 -0500

    Update Settings.java
    Added Constructor

--- src/classes/Settings.java
+++ src/classes/Settings.java
@@ -4,5 +4,10 @@ import java.io.Serializable;
 import application.*;
 
 public class Settings implements Serializable {
-
+  public boolean alertOnDelete;
+  
+  public Settings() {
+    //Default Settings
+    alertOnDelete = false;
+  }
 }
