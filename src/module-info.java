e148b6db7d85a8f7b5c272deaf690934126e1dc4
LittleTealeaf <ShintaoMonk@gmail.com>
2021-08-31 12:30:50 -0400

    Debugging Javafx
    Updated Java Language Level Version
    Created module-info.java (mostly copied from 0.1.2)

---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+module paceManager {
+    exports ui to javafx.graphics;
+
+    requires javafx.base;
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires java.desktop;
+    requires java.base;
+    requires com.google.gson;
+}
