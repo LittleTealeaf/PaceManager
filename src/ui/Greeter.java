10b1bafc1f33af74706204d51a4e0b296eca4070
LittleTealeaf <ShintaoMonk@gmail.com>
2021-08-31 12:41:45 -0400

    Began work on User Interface
    Created Greeter.java
    Defaulted to Greeter to launch first

---
+++ src/ui/Greeter.java
@@ -0,0 +1,15 @@
+package ui;
+
+import javafx.stage.Stage;
+
+public class Greeter {
+
+    private static Stage stage;
+
+    public static void open() {
+        stage = new Stage();
+
+        stage.show();
+    }
+
+}
