fbf859a0e5d043a5b7d9a021402396b967d13ba2
LittleTealeaf <ShintaoMonk@gmail.com>
2021-09-02 11:04:24 -0400

    Generated Javadocs

--- javadocs/module-search-index.js
+++ javadocs/module-search-index.js
@@ -1,1 +1,1 @@
-moduleSearchIndex = [];updateSearchResults();
+moduleSearchIndex = [{"l":"paceManager"}];updateSearchResults();
--- javadocs/package-search-index.js
+++ javadocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"data"},{"l":"main"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"paceManager","l":"app"},{"m":"paceManager","l":"data"},{"m":"paceManager","l":"ui"}];updateSearchResults();
