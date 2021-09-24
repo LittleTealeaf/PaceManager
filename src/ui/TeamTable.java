b57d08630240acb8e25987545045f546c58a8505
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2021-09-24 17:06:20 -0400

    Added Constructor to TeamTable.java

--- src/ui/TeamTable.java
+++ src/ui/TeamTable.java
@@ -1,8 +1,19 @@
 package ui;
 
+import data.Pace;
 import data.Team;
 import javafx.scene.control.TableView;
 
+/**
+ * @author Thomas Kwashnak
+ * @version 1.0.0-tableview
+ * @since 1.0.0-tableview
+ */
 public class TeamTable extends TableView<Team> {
+    private final Pace pace;
 
+    public TeamTable(Pace pace) {
+        super();
+        this.pace = pace;
+    }
 }
