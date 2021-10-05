d036cf9bd95333e59de6c4c12dd18a25cbb9e2f8
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2021-10-05 13:23:17 -0400

    Added comments to Updatable.java

--- src/app/Updatable.java
+++ src/app/Updatable.java
@@ -1,8 +1,16 @@
 package app;
 
+/**
+ * Indicates that an object is updatable, and thus has a {@link #update()} method
+ * @since 1.0.0
+ * @version 1.0.0
+ * @author Thomas Kwashnak
+ * @see App#update()
+ */
 public interface Updatable {
     /**
-     * Updates values within the updatable object
+     * This method is called whenever the application pings an update, if the object is stored within the list in {@link App},
+     * and should contain commands that updates values within the object
      */
     void update();
 }
