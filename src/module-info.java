2c048fa6045284f6ebdb2572e1e79c15d4075a6f
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2021-12-19 23:17:12 -0500

    Fixed module-info.java

--- src/module-info.java
+++ src/module-info.java
@@ -1,10 +1,3 @@
 module paceManager {
-
-    opens data_deprecated to com.google.gson;
-
-    exports data_deprecated to com.google.gson;
-    exports interfaces to com.google.gson;
-
-    requires com.google.gson;
     requires org.jetbrains.annotations;
 }
