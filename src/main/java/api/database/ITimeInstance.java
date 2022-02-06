ce2b5b9ead4ea214c5359b2f555c88ad2b898c02
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-02-06 09:48:45 -0500

    Created TimeInstance

--- src/main/java/api/database/ITimeInstance.java
+++ src/main/java/api/database/ITimeInstance.java
@@ -1,5 +1,27 @@
 package api.database;
 
 public interface ITimeInstance {
+    int getTime();
+    void setTime(int time);
+    String asString();
 
+    void add(int time);
+
+    default void add(ITimeInstance time) {
+        add(time.getTime());
+    }
+
+    default void subtract(int time) {
+        add(-time);
+    }
+
+    default void subtract(ITimeInstance time) {
+        subtract(time.getTime());
+    }
+
+    ITimeInstance getNegative();
+
+    void absolute();
+
+    ITimeInstance getAbsolute();
 }
---
+++ src/main/java/app/database/TimeInstance.java
@@ -0,0 +1,55 @@
+package app.database;
+
+import api.database.ITimeInstance;
+
+public class TimeInstance implements ITimeInstance {
+
+    private int time;
+
+    public TimeInstance() {
+        time = 0;
+    }
+
+    public TimeInstance(int time) {
+        this.time = time;
+    }
+
+    public TimeInstance(ITimeInstance other) {
+        this.time = other.getTime();
+    }
+
+    @Override
+    public int getTime() {
+        return time;
+    }
+
+    @Override
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    @Override
+    public String asString() {
+        return null;
+    }
+
+    @Override
+    public void add(int time) {
+        this.time += time;
+    }
+
+    @Override
+    public ITimeInstance getNegative() {
+        return new TimeInstance(-time);
+    }
+
+    @Override
+    public void absolute() {
+        time = Math.abs(time);
+    }
+
+    @Override
+    public ITimeInstance getAbsolute() {
+        return new TimeInstance(Math.abs(time));
+    }
+}
