0fa70eb14f6c7e22a15ee7dff600857e8d087faa
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-02-07 14:45:47 -0500

    oh, so Gradle can do it?

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,8 +1,11 @@
 package app;
 
+import java.io.File;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        File file = new File("docs");
+        System.out.println(file.getPath());
     }
 }
