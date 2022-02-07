165eff8cfb77b5ecfb95d3b759f08709285d6f91
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-02-07 13:59:22 -0500

    IClock

--- src/main/java/api/database/IClock.java
+++ src/main/java/api/database/IClock.java
@@ -1,5 +1,36 @@
 package api.database;
 
+
 public interface IClock {
 
+    /**
+     * The current time, in seconds since 00:00:00 (Midnight)
+     * @return The time represented by this clock
+     */
+    int getTime();
+
+    /**
+     * Converts the Clock time into a human-readable format
+     * @return String representation of the clock time
+     */
+    String asString();
+
+    /**
+     * Calculates the addition of two times together
+     * @param other Clock time to add to this clock time
+     * @return A new clock instance with a value of the sum of this clock and the provided clock
+     */
+    IClock getAdd(IClock other);
+    /**
+     * Calculates the subtraction of two times together
+     * @param other Clock time to subtract to this clock time
+     * @return A new clock instance with a value of the difference of this clock and the provided clock
+     */
+    IClock getSubtract(IClock other);
+
+    /**
+     * Converts this clock into its negative counterpart
+     * @return A clock instance with the negative value of this clock
+     */
+    IClock getNegative();
 }
