e7060b4116b04def56bc88f00e1f949ba9a116f1
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-02-07 15:21:57 -0500

    Create Documentation

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"api.database"},{"l":"api.util"},{"l":"app"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"api.database"},{"l":"api.util"},{"l":"app"}];updateSearchResults();
