09b86b7a2d0c90d41ea7ef706c816f7747737ffa
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-02-25 11:57:05 -0500

    Delete and Rebuild!

--- src/main/java/api/data/IPace.java
+++ src/main/java/api/data/IPace.java
@@ -1,23 +1,3 @@
 package api.data;
 
-import api.util.PaceComponent;
-
-import java.util.List;
-import java.util.UUID;
-
-public interface IPace {
-
-    List<IDivision> getDivisions();
-
-    List<ITeam> getTeams();
-
-    IDivision lookupDivision(UUID uuid);
-
-    default void initializeReferences() {
-        for (PaceComponent component : getPaceComponents()) {
-            component.setPace(this);
-        }
-    }
-
-    List<PaceComponent> getPaceComponents();
-}
+public interface IPace {}
