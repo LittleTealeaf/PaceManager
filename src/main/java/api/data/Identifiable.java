1fc53d253af194a650853b5254cd08197875f0b7
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-02-27 13:39:48 -0500

    Added Comments to Identifiable and PaceComponent

--- src/main/java/api/data/Identifiable.java
+++ src/main/java/api/data/Identifiable.java
@@ -2,9 +2,19 @@ package api.data;
 
 import java.util.UUID;
 
+/**
+ * Indicates that a class has a unique identifier, or a UUID.
+ */
 public interface Identifiable {
 
+    /**
+     * Classpath Tag for the UUID parameter
+     */
     String UUID = Identifiable.class.getName() + ".UUID";
 
+    /**
+     * The unique identifier of the object.
+     * @return A UUID uniquely identifying the object it was created under.
+     */
     UUID getUUID();
 }
--- src/main/java/api/data/PaceComponent.java
+++ src/main/java/api/data/PaceComponent.java
@@ -1,7 +1,21 @@
 package api.data;
 
+/**
+ * Indicates that an object is part of an {@link IPace}, and should have a back-reference to the {@link IPace} it is a part of
+ * @author Thomas Kwashnak
+ * @since 2.0.0
+ */
 public interface PaceComponent {
+
+    /**
+     * Gets the pace that this object is a part of
+     * @return The IPace object
+     */
     IPace getPace();
 
+    /**
+     * Updates the object's current pace
+     * @param pace The pace this object is a part of
+     */
     void setPace(IPace pace);
 }
