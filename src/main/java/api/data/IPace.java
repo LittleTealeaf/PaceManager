6bbb0ed2d1193352342a2e032a586d473e58dfa2
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-02-26 20:26:48 -0500

    Created outline of IPace

--- src/main/java/api/data/IPace.java
+++ src/main/java/api/data/IPace.java
@@ -1,5 +1,6 @@
 package api.data;
 
+import java.util.Collection;
 import java.util.UUID;
 
 /**
@@ -11,4 +12,24 @@ public interface IPace {
     ITeam getTeam(UUID uuid);
 
     IDivision getDivision(UUID uuid);
+
+    Collection<ITeam> getTeams();
+
+    Collection<IDivision> getDivisions();
+
+    void addTeam(ITeam team);
+
+    boolean removeTeam(ITeam team);
+
+    void addDivision(IDivision division);
+
+    boolean removeDivision(IDivision division);
+
+    default void addPaceComponent(PaceComponent component) {
+        component.setPace(this);
+    }
+
+    default void removePaceComponent(PaceComponent component) {
+        component.setPace(null);
+    }
 }
--- src/main/java/api/data/PaceComponent.java
+++ src/main/java/api/data/PaceComponent.java
@@ -1,5 +1,7 @@
 package api.data;
 
 public interface PaceComponent {
+    IPace getPace();
 
+    void setPace(IPace pace);
 }
