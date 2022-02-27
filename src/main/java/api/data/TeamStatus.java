5078252d1e113bea9ddcb82722d4756eaf553260
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-02-27 15:30:12 -0500

    Added Comments to TeamStatus.java

--- src/main/java/api/data/TeamStatus.java
+++ src/main/java/api/data/TeamStatus.java
@@ -1,5 +1,19 @@
 package api.data;
 
+/**
+ * The current status of a given Team
+ */
 public enum TeamStatus {
-    NOT_STARTED,IN_PROGRESS,FINISHED;
+    /**
+    Indicates that the team has not yet started the pace
+     */
+    NOT_STARTED,
+    /**
+    Indicates that the team is currently in progress of completing the pace.
+     */
+    IN_PROGRESS,
+    /**
+    Indicates that the team has completed the pace.
+     */
+    FINISHED;
 }
