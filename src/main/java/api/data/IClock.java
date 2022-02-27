59abb0467b103120e630de30bad76cdc4d078cb5
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-02-27 13:10:26 -0500

    Added Comments

--- src/main/java/api/data/IClock.java
+++ src/main/java/api/data/IClock.java
@@ -1,9 +1,16 @@
 package api.data;
 
+/**
+ * @author Thomas Kwashnak
+ */
 public interface IClock {
 
     String TIME = IClock.class.getName() + ":TIME";
 
+    /**
+     *
+     * @return Number of seconds since 00:00 (midnight)
+     */
     int getTime();
 
     void setTime();
