055ef347ab3dbf9fc673920ba6bc8ae80cc18623
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-02-27 13:57:31 -0500

    Added ITeam values

--- src/main/java/api/data/ITeam.java
+++ src/main/java/api/data/ITeam.java
@@ -1,13 +1,17 @@
 package api.data;
 
-import java.util.UUID;
-
 /**
  * @author Thomas Kwashnak
  * @since 2.0.0
  * @version 2.0.0
  */
 public interface ITeam extends Identifiable, PaceComponent {
+
+    String KEY_NAME = ITeam.class.getName() + ".name";
+    String KEY_DIVISION_UUID = ITeam.class.getName() + ".divisionUUID";
+    String KEY_START_TIME = ITeam.class.getName() + ".timeStart";
+    String KEY_END_TIME = ITeam.class.getName() + ".timeEnd";
+    String KEY_INCLUDED = ITeam.class.getName() + ".included";
 
     String getName();
     void setName(String name);
@@ -24,4 +28,7 @@ public interface ITeam extends Identifiable, PaceComponent {
 
     IClock getElapsedTime();
     TeamStatus getStatus();
+
+    boolean isIncluded();
+    void setIncluded(boolean included);
 }
