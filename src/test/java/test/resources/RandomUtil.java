e908f94b4be61004455b671e8a9de3e0189466f7
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-03-02 14:52:09 -0500

    Added Documentation to RandomUtil.java and RandomUtils.java

--- src/test/java/test/resources/RandomUtil.java
+++ src/test/java/test/resources/RandomUtil.java
@@ -2,6 +2,13 @@ package test.resources;
 
 import java.util.Random;
 
+/**
+ * Provides a static random object to use during testing
+ */
 public interface RandomUtil {
+
+    /**
+     * An instance of the {@link Random} object
+     */
     Random RANDOM = new Random();
 }
