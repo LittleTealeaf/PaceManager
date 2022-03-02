576e394f7c77c597af667c7749d2face05a714c2
LittleTealeaf <LittleTealeaf@users.noreply.github.com>
2022-03-02 16:48:17 +0000

    deploy: 4d4309af46d134a68824ea3ee0cc6bef393af09c

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"api.data"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"app"},{"l":"data.api"},{"l":"data.interfaces"},{"l":"data.structure"},{"l":"util"}]
