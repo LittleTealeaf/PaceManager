0485a4d0e4162a02719c49dc1290b816fce1cd9a
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-03-03 14:56:53 -0500

    Added division from UUID test

--- src/test/java/data/structure/TeamTest.java
+++ src/test/java/data/structure/TeamTest.java
@@ -1,14 +1,11 @@
 package data.structure;
 
-import data.api.IClock;
 import data.api.IDivision;
 import data.api.IPace;
 import data.interfaces.PaceComponentTest;
 import org.junit.jupiter.api.Test;
 import test.resources.RandomUtils;
 import test.resources.TestIdentifiable;
-
-import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -23,49 +20,16 @@ public class TeamTest implements TestIdentifiable, PaceComponentTest {
         return new Pace();
     }
 
-    /**
-     * Creates a new Division object
-     *
-     * @return New Division object
-     */
-    private static IDivision newDivision() {
-        UUID uuid = UUID.randomUUID();
-        return new IDivision() {
-            @Override
-            public String getName() {
-                return null;
-            }
-
-            @Override
-            public void setName(String name) {
-
-            }
-
-            @Override
-            public IClock getGoalTime() {
-                return null;
-            }
-
-            @Override
-            public void setGoalTime(IClock time) {
-
-            }
-
-            @Override
-            public UUID getUUID() {
-                return uuid;
-            }
-
-            @Override
-            public IPace getPace() {
-                return null;
-            }
-
-            @Override
-            public void setPace(IPace pace) {
-
-            }
-        };
+    @Test
+    public void testDivisionUUID() {
+        Team team = new Team();
+        IDivision division = newDivision();
+        assertNotNull(division);
+        IPace pace = newPace();
+        pace.addDivision(division);
+        team.setPace(pace);
+        team.setDivision(division.getUUID());
+        assertEquals(division, team.getDivision());
     }
 
     @Test
@@ -104,5 +68,14 @@ public class TeamTest implements TestIdentifiable, PaceComponentTest {
     public void testNullDivision() {
         Team team = new Team();
         assertNull(team.getDivision());
+    }
+
+    /**
+     * Creates a new Division object
+     *
+     * @return New Division object
+     */
+    private static IDivision newDivision() {
+        return null;
     }
 }
