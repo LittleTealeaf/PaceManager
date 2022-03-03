fa497bd87304087d01fc622598ca222a17b36524
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-03-03 14:50:51 -0500

    Added newPace and newDivision objects

--- src/test/java/data/structure/TeamTest.java
+++ src/test/java/data/structure/TeamTest.java
@@ -1,13 +1,72 @@
 package data.structure;
 
+import data.api.IClock;
+import data.api.IDivision;
+import data.api.IPace;
 import data.interfaces.PaceComponentTest;
 import org.junit.jupiter.api.Test;
 import test.resources.RandomUtils;
 import test.resources.TestIdentifiable;
 
+import java.util.UUID;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TeamTest implements TestIdentifiable, PaceComponentTest {
+
+    /**
+     * Creates a new Pace object
+     *
+     * @return New Pace object
+     */
+    private static IPace newPace() {
+        return new Pace();
+    }
+
+    /**
+     * Creates a new Division object
+     *
+     * @return New Division object
+     */
+    private static IDivision newDivision() {
+        UUID uuid = UUID.randomUUID();
+        return new IDivision() {
+            @Override
+            public String getName() {
+                return null;
+            }
+
+            @Override
+            public void setName(String name) {
+
+            }
+
+            @Override
+            public IClock getGoalTime() {
+                return null;
+            }
+
+            @Override
+            public void setGoalTime(IClock time) {
+
+            }
+
+            @Override
+            public UUID getUUID() {
+                return uuid;
+            }
+
+            @Override
+            public IPace getPace() {
+                return null;
+            }
+
+            @Override
+            public void setPace(IPace pace) {
+
+            }
+        };
+    }
 
     @Test
     @Override
@@ -42,7 +101,7 @@ public class TeamTest implements TestIdentifiable, PaceComponentTest {
     }
 
     @Test
-    public void testDivision() {
+    public void testNullDivision() {
         Team team = new Team();
         assertNull(team.getDivision());
     }
