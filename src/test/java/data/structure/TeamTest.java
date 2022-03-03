cfdd58da1713e23810d07234af5c71081276e6b9
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-03-03 14:35:51 -0500

    TeamTest implements PaceComponentTest

--- src/test/java/data/structure/TeamTest.java
+++ src/test/java/data/structure/TeamTest.java
@@ -1,13 +1,20 @@
 package data.structure;
 
+import data.interfaces.PaceComponentTest;
 import org.junit.jupiter.api.Test;
 import test.resources.TestIdentifiable;
 
-class TeamTest implements TestIdentifiable {
+public class TeamTest implements TestIdentifiable, PaceComponentTest {
 
     @Test
     @Override
     public void testIdentifiable() {
         doIdentifiableTests(Team::new);
+    }
+
+    @Test
+    @Override
+    public void testPaceComponent() {
+        doPaceComponentTests(Team::new);
     }
 }
