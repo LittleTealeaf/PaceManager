9ed19266038cbbbb7de0ea93a895cd158041c92c
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-03-06 10:43:15 -0500

    Made ClassUtil final and added private constructor

--- src/main/java/util/ClassUtil.java
+++ src/main/java/util/ClassUtil.java
@@ -1,6 +1,8 @@
 package util;
 
-public class ClassUtil {
+public final class ClassUtil {
+
+    private ClassUtil() {}
 
     public static String formatKey(Class varClass, String name) {
         return varClass.getName() + "." + name;
