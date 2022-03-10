39bc5ca60dbd05e1a6b9761d7e035f221b4873ec
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-03-10 14:31:20 -0500

    remove commented code

--- pages.sh
+++ pages.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# Build github pages
-# chmod +x ./gradlew
-# ./gradlew javadoc
-
 # Move docs over to the pages subdirectory
 rm -r ./pages/javadoc
 cp -r ./build/docs/javadoc ./pages/javadoc
