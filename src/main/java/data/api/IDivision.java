e74d45bb6694e385843028f869f2d6757327ebf0
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-03-24 22:38:39 -0400

    started adding comments to division

--- src/main/java/data/api/IDivision.java
+++ src/main/java/data/api/IDivision.java
@@ -5,12 +5,28 @@ import data.interfaces.PaceComponent;
 
 import java.util.List;
 
+/**
+ * Represents a division of riders with a goal time
+ * @author Thomas Kwashnak
+ */
 public interface IDivision extends Identifiable, PaceComponent {
 
+    /**
+     * Gets the display name of the division
+     * @return Name of the division
+     */
     String getName();
 
+    /**
+     * Sets the display name of the division
+     * @param name Display name of the division
+     */
     void setName(String name);
 
+    /**
+     * 
+     * @return
+     */
     IClock getGoalTime();
 
     void setGoalTime(IClock goalTime);
