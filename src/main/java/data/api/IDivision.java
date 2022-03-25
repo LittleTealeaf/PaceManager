6e748f12a513350fd1d8c991aa8fe786c1e248fe
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-03-24 22:49:01 -0400

    finish commenting IDivision.java

--- src/main/java/data/api/IDivision.java
+++ src/main/java/data/api/IDivision.java
@@ -24,14 +24,26 @@ public interface IDivision extends Identifiable, PaceComponent {
     void setName(String name);
 
     /**
-     * 
-     * @return
+     * Gets the current goal time that riders within the division should aim for
+     * @return The current goal time of the division
      */
     IClock getGoalTime();
 
+    /**
+     * Sets the goal time that riders within the division should aim for
+     * @param goalTime The new goal time
+     */
     void setGoalTime(IClock goalTime);
 
+    /**
+     * Gets the list of teams that are currently stored within the division
+     * @return List of teams in the division
+     */
     List<ITeam> getTeams();
 
+    /**
+     * Checks the associated IPace to see if this is the default division
+     * @return {@code true} if this division is the default division, {@code false} otherwise.
+     */
     boolean isDefaultDivision();
 }
