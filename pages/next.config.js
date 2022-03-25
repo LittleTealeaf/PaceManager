512dbc986fb0f48da35563e841cd34f0bfa276e4
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-03-25 09:46:50 -0400

    Remove old config

--- pages/next.config.js
+++ pages/next.config.js
@@ -1,27 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   images: {
-//     disableStaticImages: true
-//   },
-//   exportPathMap: async function(
-//     defaultPathMap,
-//     {dev,dir,outDir,distDir,buildId}
-//   ) {
-//     return {
-//       '/': { page: '/'},
-//       '/javadoc': { page: '/javadoc' }
-//     }
-//   }
-// }
-
-// const withPlugins = require('next-compose-plugins');
-
-// module.exports = withPlugins([
-//   [require('next-optimized-images'),{}]
-// ],nextConfig)
-
-
 const withPlugins = require('next-compose-plugins');
 
 
