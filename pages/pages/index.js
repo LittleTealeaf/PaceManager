83a1551511a570cd1804d45ee91b02032c7ebfba
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-03-25 10:52:45 -0400

    Added basic homepage

--- pages/pages/index.js
+++ pages/pages/index.js
@@ -1,9 +1,11 @@
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div>
-      Hello
+      Hey, This page is currently under progress, so... Here's a link to the: <br />
+      <Link href='/javadoc/index.html' passHref><a className='to-blue-500'>Javadocs</a></Link>
     </div>
   )
 }
