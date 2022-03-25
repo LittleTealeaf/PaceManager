7b3aae833c9521eb8bec0e14f924cae42d7caf90
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-03-25 09:46:08 -0400

    nextJS actually compiles

--- pages/next.config.js
+++ pages/next.config.js
@@ -1,22 +1,51 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     disableStaticImages: true
+//   },
+//   exportPathMap: async function(
+//     defaultPathMap,
+//     {dev,dir,outDir,distDir,buildId}
+//   ) {
+//     return {
+//       '/': { page: '/'},
+//       '/javadoc': { page: '/javadoc' }
+//     }
+//   }
+// }
+
+// const withPlugins = require('next-compose-plugins');
+
+// module.exports = withPlugins([
+//   [require('next-optimized-images'),{}]
+// ],nextConfig)
+
+
+const withPlugins = require('next-compose-plugins');
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.module.rules.push({
+      test: /\.html$/i,
+      use: [{
+        loader: "html-loader",
+        options:{}
+      }]
+    })
+    // Important: return the modified config
+    return config
+  },
   reactStrictMode: true,
   images: {
     disableStaticImages: true
   },
-  exportPathMap: async function(
-    defaultPathMap,
-    {dev,dir,outDir,distDir,buildId}
-  ) {
-    return {
-      '/': { page: '/'},
-      '/javadoc': { page: '/javadoc' }
-    }
-  }
+  basePath: '/paceManager'
 }
-
-const withPlugins = require('next-compose-plugins');
 
 module.exports = withPlugins([
   [require('next-optimized-images'),{}]
-],nextConfig)
+],nextConfig);
+
