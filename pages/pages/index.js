a4446e8e6d98dc7383077d223c60d34eb478524f
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-03-25 10:55:28 -0400

    fix compile

--- pages/pages/index.js
+++ pages/pages/index.js
@@ -4,8 +4,12 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div>
-      Hey, This page is currently under progress, so... Here's a link to the: <br />
+      <div>
+      {"Hey, This page is currently under progress, so... Here's a link to the:"}
+      </div>
+      <div>
       <Link href='/javadoc/index.html' passHref><a className='to-blue-500'>Javadocs</a></Link>
+      </div>
     </div>
   )
 }
