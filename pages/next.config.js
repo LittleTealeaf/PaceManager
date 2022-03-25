0d5730324277b2ed10fae6610362cef1f7afa926
Thomas Kwashnak <shintaomonk@gmail.com>
2022-03-25 09:17:14 -0400

    well then, things are gonna be kinda broken?

--- pages/next.config.js
+++ pages/next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    disableStaticImages: true
+  },
+  exportPathMap: async function(
+    defaultPathMap,
+    {dev,dir,outDir,distDir,buildId}
+  ) {
+    return {
+      '/': { page: '/'},
+      '/javadoc': { page: '/javadoc' }
+    }
+  }
 }
 
-module.exports = nextConfig
+const withPlugins = require('next-compose-plugins');
+
+module.exports = withPlugins([
+  [require('next-optimized-images'),{}]
+],nextConfig)
