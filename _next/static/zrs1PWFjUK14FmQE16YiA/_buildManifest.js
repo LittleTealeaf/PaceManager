47b646b18ebeddf81f12210ae6212540ee655402
LittleTealeaf <LittleTealeaf@users.noreply.github.com>
2022-04-14 14:29:12 +0000

    deploy: 6dccb3e76f4d287fcb97f590c1b76567c295e3b4

--- _next/static/zrs1PWFjUK14FmQE16YiA/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-0c66e7de4de2be2a.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
