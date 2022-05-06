b11708faaed1e4a0aefda38b9842a908f6452b6a
LittleTealeaf <thomaskwashnak@gmail.com>
2022-05-06 13:28:50 -0400

    Updates!

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module org.tealeaf.pacemanager {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens org.tealeaf.pacemanager to javafx.fxml;
+    exports org.tealeaf.pacemanager;
+}
