28b96c03cceb4f98f46b8521a54249d156954a0f
LittleTealeaf <thomaskwashnak@gmail.com>
2022-05-06 13:54:33 -0400

    Fixed Stuff

---
+++ src/test/java/org/tealeaf/pacemanager/HelloApplicationTest.java
@@ -0,0 +1,14 @@
+package org.tealeaf.pacemanager;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.toolkit.ApplicationLauncher;
+
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(ApplicationExtension.class)
+public class HelloApplicationTest {
+
+}
