6056338824f1a95b799b2b6444c17ea32c1fe8c6
Thomas <thomaskwashnak@gmail.com>
2022-05-07 19:41:45 -0400

    Test Removing Dependencies

--- src/test/java/org/tealeaf/pacemanager/app/PaceManagerTest.java
+++ src/test/java/org/tealeaf/pacemanager/app/PaceManagerTest.java
@@ -27,14 +27,6 @@ public class PaceManagerTest extends ApplicationTest {
 
     @BeforeEach
     public void beforeEachTest() throws Exception {
-        if (Boolean.getBoolean("headless")) {
-            System.setProperty("testfx.robot", "glass");
-            System.setProperty("testfx.headless", "true");
-            System.setProperty("prism.order", "sw");
-            System.setProperty("prism.text", "t2k");
-            System.setProperty("java.awt.headless", "true");
-        }
-
 
         ApplicationTest.launch(PaceManager.class);
     }
@@ -58,6 +50,6 @@ public class PaceManagerTest extends ApplicationTest {
 
     @Test
     public void testLaunching() {
-
+        assertFalse(true);
     }
 }
