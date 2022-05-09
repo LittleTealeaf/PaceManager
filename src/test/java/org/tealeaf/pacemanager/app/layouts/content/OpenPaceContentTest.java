d84e6c9488ae44d76472b57b6270e37dd86f1a2b
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-05-08 23:36:27 -0400

    Fixed Test

--- src/test/java/org/tealeaf/pacemanager/app/layouts/content/OpenPaceContentTest.java
+++ src/test/java/org/tealeaf/pacemanager/app/layouts/content/OpenPaceContentTest.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 import org.tealeaf.pacemanager.app.Identifier;
 import org.tealeaf.pacemanager.events.EventManager;
+import org.tealeaf.pacemanager.system.GsonWrapper;
 import test.org.tealeaf.pacemanager.UserInterfaceTest;
 import test.org.tealeaf.pacemanager.database.dataobjects.RandomDataObjects;
 
+import java.io.File;
 import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -15,7 +17,10 @@ class OpenPaceContentTest extends UserInterfaceTest implements RandomDataObjects
 
     @Test
     void testOpen(@TempDir Path tempPath) throws Exception {
-        actionOpenPaceFile(randomPace(new EventManager()), tempPath.resolve("test.json"));
+        File file = tempPath.resolve("abc.json").toFile();
+        GsonWrapper.write(randomPace(new EventManager()), file);
+        clickOn(Identifier.LAYOUT_CONTENT_OPEN_PACE_BUTTON);
+        applyPath(file.getPath());
         assertTrue(exists(Identifier.LAYOUT_CONTENT_APP));
     }
 }
